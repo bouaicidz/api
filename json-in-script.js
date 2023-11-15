4c8eca2ba40074d401d9334f0df200443b59814e
bouaicidz <39716681+bouaicidz@users.noreply.github.com>
2023-11-15 21:00:19 +0100

    json-in-script.js

--- json-in-script.js
+++ json-in-script.js
@@ -1,26 +1,5 @@
 // API callback
 DFSDX({
-  "version": "1.0",
-  "encoding": "UTF-8",
-  "entry": {
-    "xmlns": "http://www.w3.org/2005/Atom",
-    "xmlns$gd": "http://schemas.google.com/g/2005",
-    "xmlns$georss": "http://www.georss.org/georss",
-    "xmlns$thr": "http://purl.org/syndication/thread/1.0",
-    "xmlns$blogger": "http://schemas.google.com/blogger/2008",
-    "id": {
-      "$t": "tag:blogger.com,1999:blog-6815744020569538844"
-    },
-    "published": {
-      "$t": ""
-    },
-    "updated": {
-      "$t": "2023-11-14"
-    },
-    "title": {
-      "type": "text",
-      "$t": "css"
-    },
     "content": {
       "type": "html",
       "$t": "<!--.created-using-layers{position:absolute!important;right:0!important;top:0!important;width:32px!important;height:32px!important;margin:10px!important}.created-using-layers a{background:url(https://yt3.ggpht.com/t7DCCaCoRkS1wm_sq-CjY_o0rx7Bvx5JUkyuqZIZI5j87Fi0qAt0PDqIONsHRL3pp8QRZ1SsYzY=s176-c-k-c0x00ffffff-no-rj-mo) top no-repeat!important;width:32px!important;height:32px!important;background-size:32px 32px!important;-webkit-transition:all 0.15s ease-in-out;-moz-transition:all 0.15s ease-in-out;-ms-transition:all 0.15s ease-in-out;-o-transition:all 0.15s ease-in-out;transition:all 0.15s ease-in-out}.created-using-layers a:after{font-family:sans-serif;font-size:0.5rem!important;padding:2px 5px!important;position:absolute!important;top:0;right:35px;z-index:1;content:attr(tooltip);background:#4cb050;color:#ffffff;opacity:0;visibility:hidden;-webkit-transition:all 0.1s ease-in-out 0.1s;-moz-transition:all 0.1s ease-in-out 0.1s;-ms-transition:all 0.1s ease-in-out 0.1s;-o-transition:all 0.1s ease-in-out 0.1s;transition:all 0.1s ease-in-out 0.1s;-webkit-border-radius:100px;-moz-border-radius:100px;-ms-border-radius:100px;-o-border-radius:100px;border-radius:100px;line-height:0.9rem;text-transform:uppercase;letter-spacing:2px;white-space:nowrap}.created-using-layers a:hover:after{opacity:1;visibility:visible;right:30px}-->"
